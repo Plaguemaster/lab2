82d5c133b2975222aa2a555826a5a23e9e95c854
Plaguemaster <maks.maxim-kireew@yandex.ru>
2022-05-28 18:46:59 +0300

    clang style

--- lab2/Hello_world.cpp
+++ lab2/Hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-
-int main(int argc, char** argv){
-char * name ;//this is name
-std::cin >>name;//getting name
-std:: cout << "Hello world from"<<name << std::endl;//show some magic
+int
+main(int argc, char** argv)
+{
+  char* name;                                           // this is name
+  std::cin >> name;                                     // getting name
+  std::cout << "Hello world from" << name << std::endl; // show some magic
 }
-
